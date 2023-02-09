60bf1fd88cbc48913056e9675b82de294f156430
faverog <63128330+faverog@users.noreply.github.com>
2023-02-09 16:28:17 -0500

    All five finger angles outputted on image

--- Testing/Video Test/realtime_video_test.py
+++ Testing/Video Test/realtime_video_test.py
@@ -10,65 +10,68 @@ mp_hands = mp.solutions.hands
 cap = cv2.VideoCapture(0)
 
 with mp_hands.Hands(
-	model_complexity=0,
-		min_detection_confidence=0.9,
-		min_tracking_confidence=0.5) as hands:
-		
-	while cap.isOpened():
-		success, image = cap.read()
-		if not success:
-			print("Ignoring empty camera frame.")
-			# If loading a video, use 'break' instead of 'continue'.
-			continue
+    model_complexity=1,
+    min_detection_confidence=0.9,
+    min_tracking_confidence=0.5) as hands:
 
-		# To improve performance, optionally mark the image as not writeable to
-		# pass by reference.
-		image.flags.writeable = False
-		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-		results = hands.process(image)
+    finger_angles = {
+    "thumb": {
+        "pip": [], "mcp": []},
+    "index": {
+        "pip": [], "mcp": []},
+    "middle": {
+        "pip": [], "mcp": []},
+    "ring": {
+        "pip": [], "mcp": []},
+    "pinky": {
+        "pip": [], "mcp": []},
+    }
+        
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
 
-		# Draw the hand annotations on the image.
-		image.flags.writeable = True
-		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
 
-		if results.multi_hand_landmarks:
-			for hand_landmarks in results.multi_hand_landmarks:
-				landmarks = landmark_dict(hand_landmarks)
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-			mp_drawing.draw_landmarks(
-				image,
-				hand_landmarks,
-				mp_hands.HAND_CONNECTIONS,
-				mp_drawing_styles.get_default_hand_landmarks_style(),
-				mp_drawing_styles.get_default_hand_connections_style())
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                landmarks = landmark_dict(hand_landmarks)
 
-			finger_angles = {
-				"thumb": {
-					"pip": [], "mcp": []},
-				"index": {
-					"pip": [], "mcp": []},
-				"middle": {
-					"pip": [], "mcp": []},
-				"ring": {
-					"pip": [], "mcp": []},
-				"pinky": {
-					"pip": [], "mcp": []},
-			}
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
 
-			for finger in finger_angles:
-				finger_angles[finger]["pip"].append(pip_theta(landmarks, finger))
-				finger_angles[finger]["mcp"].append(mcp_theta(landmarks, finger))
-				
-				'''
-				if len(finger_angles[finger]["pip"]) > 500:
-					finger_angles[finger]["pip"].pop(0)
-					finger_angles[finger]["mcp"].pop(0)
-'''
-			print(finger_angles["middle"]["pip"])
+            for finger in finger_angles:
+                finger_angles[finger]["pip"].append(pip_theta(landmarks, finger))
+                finger_angles[finger]["mcp"].append(mcp_theta(landmarks, finger))
 
-		image = cv2.flip(image, 1)
-		cv2.imshow('MediaPipe Hands', image)
-		if cv2.waitKey(5) & 0xFF == ord('q'):
-			break
+                if len(finger_angles[finger]["pip"]) > 10:
+                    finger_angles[finger]["pip"].pop(0)
+                if len(finger_angles[finger]["mcp"]) > 10:
+                    finger_angles[finger]["mcp"].pop(0)
+
+        image = cv2.flip(image, 1)
+        cv2.putText(image, f'{"thumb"} IP {sum(finger_angles["thumb"]["pip"]) / (len(finger_angles["thumb"]["pip"])+0.001)}', (5, 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+        cv2.putText(image, f'{"index"} IP {sum(finger_angles["index"]["pip"]) / (len(finger_angles["index"]["pip"])+0.001)}', (5, 50), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+        cv2.putText(image, f'{"middle"} IP {sum(finger_angles["middle"]["pip"]) / (len(finger_angles["middle"]["pip"])+0.001)}', (5, 70), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+        cv2.putText(image, f'{"ring"} IP {sum(finger_angles["ring"]["pip"]) / (len(finger_angles["ring"]["pip"])+0.001)}', (5, 90), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+        cv2.putText(image, f'{"pinky"} IP {sum(finger_angles["pinky"]["pip"]) / (len(finger_angles["pinky"]["pip"])+0.001)}', (5, 110), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+        cv2.imshow('MediaPipe Hands', image)
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
 
 cap.release()
