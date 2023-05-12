dbc848395fce9a71bf156490fa5f8db09ee9e528
Uygur Tepe <uygur.t10@gmail.com>
2023-05-11 21:52:58 -0400

    added rest of code for BBB

--- Testing/Hall Sensor Test/Deprecated/BBB_helpers.py
+++ Testing/Hall Sensor Test/Deprecated/BBB_helpers.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+from Adafruit_BBIO import ADC
+
+# Define the ADC input pin
+hall_pin = 'P9_39'
+
+# Set up the ADC
+ADC.setup()
+
 # Setup smoothing parameters
 window_length = 10
 readings = [0] * window_length
@@ -9,7 +18,7 @@ moving_avg = 0
 # this does have slow start tho which isnt ideal
 def smoothing():
     global moving_avg, read_index, readings, index
-    reading # hall sensor reading  
+    reading = ADC.read(hall_pin) 
     print(f'Hall sensor value: {reading}')
 
     moving_avg += (1/window_length)*(reading - readings[read_index])
