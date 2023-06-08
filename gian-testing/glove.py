ddeaffd3743ec1a6b7813073036eff9a7c8c9aa9
faverog <63128330+faverog@users.noreply.github.com>
2023-06-08 01:37:46 -0400

    mcp adjustment seems to be working :)

--- gian-testing/glove.py
+++ gian-testing/glove.py
@@ -15,25 +15,27 @@ select_pins = [Pin(i, Pin.OUT) for i in select_pin_nums]
 z_mux_1 = ADC(Pin(27))    # Z1~GP27
 z_mux_2 = ADC(Pin(26))    # Z2~GP26
 
-# Configure the UART (serial port)
-uart = machine.UART(0, baudrate=9600)
-
 class WBA_Glove:
     
     def __init__(self) -> None:
         # first mux will track mcp joints, second mux will track pip joints
         self.mcp_joints = {
-            "calibration": { "thumb": [0, 1e6, 0, 0], "index": [0, 1e6, 0, 0], "middle": [0, 1e6, 0, 0], "ring": [0, 1e6, 0, 0], "pinky": [0, 1e6, 0, 0] }, # [max, min, m, b]
+            # [max, min, m, b]
+            "calibration": { "thumb": [0, 1e6, 0, 0], "index": [0, 1e6, 0, 0], "middle": [0, 1e6, 0, 0], "ring": [0, 1e6, 0, 0], "pinky": [0, 1e6, 0, 0] }, 
             "raw": { "thumb": [], "index": [], "middle": [], "ring": [], "pinky": [] },
             "current_avg": { "thumb": 0, "index": 0, "middle": 0, "ring": 0, "pinky": 0 },
             "angle": { "thumb": 0, "index": 0, "middle": 0, "ring": 0, "pinky": 0 }
             }
         
         self.pip_joints = {
-            "calibration": { "thumb": [0, 1e6, 0, 0], "index": [0, 1e6, 0, 0], "middle": [0, 1e6, 0, 0], "ring": [0, 1e6, 0, 0], "pinky": [0, 1e6, 0, 0] }, # [max, min, m, b]
+            # [max, min, m, b]
+            "mcp_adjustment": { "thumb": [0, 1e6, 0, 0], "index": [0, 1e6, 0, 0], "middle": [0, 1e6, 0, 0], "ring": [0, 1e6, 0, 0], "pinky": [0, 1e6, 0, 0] }, 
+            # [max, min, m, b]
+            "calibration": { "thumb": [0, 1e6, 0, 0], "index": [0, 1e6, 0, 0], "middle": [0, 1e6, 0, 0], "ring": [0, 1e6, 0, 0], "pinky": [0, 1e6, 0, 0] }, 
             "raw": { "thumb": [], "index": [], "middle": [], "ring": [], "pinky": [] },
             "current_avg": { "thumb": 0, "index": 0, "middle": 0, "ring": 0, "pinky": 0 },
-            "angle": { "thumb": 0, "index": 0, "middle": 0, "ring": 0, "pinky": 0 }
+            "angle": { "thumb": 0, "index": 0, "middle": 0, "ring": 0, "pinky": 0 },
+            "adjustment": { "thumb": 0, "index": 0, "middle": 0, "ring": 0, "pinky": 0 }
             }
     
     def select_pin(self, p, pins): 
@@ -61,25 +63,92 @@ class WBA_Glove:
             if time_ns() % sampling_time < sampling_time / 2:
                 i+=1
                 
-    def update_calib_data(self):
+    def calibrate(self):
+        state = ["OPEN", "CLOSE"]
+        
+        print("PIP")
+        
+        # Gather data to create PIP model
+        i=0
+        time = time_ns()
+        while i < 5:
+            self.read()
+            self.update_PIP_extrema()
+            
+            if time_ns() - time > 2e9:
+                time = time_ns()
+                print(state[i % 2])
+                i += 1
+
         for finger in fingers:
-            curr_mcp_max = glove.mcp_joints["calibration"][finger][0]
-            curr_mcp_min = glove.mcp_joints["calibration"][finger][1]
-            self.mcp_joints["calibration"][finger][0] = max(self.mcp_joints["calibration"][finger][0], self.mcp_joints["raw"][finger][-1])
-            self.mcp_joints["calibration"][finger][1] = min(self.mcp_joints["calibration"][finger][1], self.mcp_joints["raw"][finger][-1])
+            self.pip_joints["calibration"][finger][2] = self.linear_fit(90, 0, self.pip_joints["calibration"][finger][0],
+                                                                        self.pip_joints["calibration"][finger][1])[0]
+            self.pip_joints["calibration"][finger][3] = self.linear_fit(90, 0, self.pip_joints["calibration"][finger][0],
+                                                                        self.pip_joints["calibration"][finger][1])[1]
+            self.pip_joints["raw"][finger] = []
+        print("PIP Calibration Complete")
+        input("Press Enter to continue...")
+        print("MCP")
+        
+        # Gather data to create MCP model
+        i=0
+        time = time_ns()
+        while i < 5:
+            self.read()
+            self.update_MCP_extrema()
             
-            curr_pip_max = self.pip_joints["calibration"][finger][0]
-            curr_pip_min = self.pip_joints["calibration"][finger][1]
-            self.pip_joints["calibration"][finger][0] = max(self.pip_joints["calibration"][finger][0], self.pip_joints["raw"][finger][-1])
-            self.pip_joints["calibration"][finger][1] = min(self.pip_joints["calibration"][finger][1], self.pip_joints["raw"][finger][-1])
-            
-    def curve_fit(self):
+            if time_ns() - time > 2e9:
+                time = time_ns()
+                print(state[i % 2])
+                i += 1
+                
         for finger in fingers:
-            self.mcp_joints["calibration"][finger][2] = 90 / (self.mcp_joints["calibration"][finger][1] - self.mcp_joints["calibration"][finger][0]) # m
-            self.mcp_joints["calibration"][finger][3] = 90 - self.mcp_joints["calibration"][finger][2] * self.mcp_joints["calibration"][finger][1] # b
+            self.mcp_joints["calibration"][finger][2] = self.linear_fit(0, 90, self.mcp_joints["calibration"][finger][0],
+                                                                        self.mcp_joints["calibration"][finger][1])[0]
+            self.mcp_joints["calibration"][finger][3] = self.linear_fit(0, 90, self.mcp_joints["calibration"][finger][0],
+                                                                        self.mcp_joints["calibration"][finger][1])[1]
             
-            self.pip_joints["calibration"][finger][2] = 90 / (self.pip_joints["calibration"][finger][1] - self.pip_joints["calibration"][finger][0]) # m
-            self.pip_joints["calibration"][finger][3] = 90 - self.pip_joints["calibration"][finger][2] * self.pip_joints["calibration"][finger][1] # b
+            # Adjust MCP interference with PIP
+            max_pip_adjustment = self.linear_func(self.pip_joints["mcp_adjustment"][finger][0],
+                                                  self.pip_joints["calibration"][finger][2],
+                                                  self.pip_joints["calibration"][finger][3])
+            min_pip_adjustment = self.linear_func(self.pip_joints["mcp_adjustment"][finger][1],
+                                                  self.pip_joints["calibration"][finger][2],
+                                                  self.pip_joints["calibration"][finger][3])
+            
+            print(finger, max_pip_adjustment, min_pip_adjustment)
+            
+            [self.pip_joints["mcp_adjustment"][finger][2], self.pip_joints["mcp_adjustment"][finger][3]] = self.linear_fit(max_pip_adjustment, min_pip_adjustment,
+                                                                                                                           self.mcp_joints["calibration"][finger][0],
+                                                                                                                           self.mcp_joints["calibration"][finger][1])
+                
+    def update_MCP_extrema(self):
+        for finger in fingers:
+            self.mcp_joints["calibration"][finger][0] = max(self.mcp_joints["calibration"][finger][0], 
+                                                            self.mcp_joints["current_avg"][finger])
+            self.mcp_joints["calibration"][finger][1] = min(self.mcp_joints["calibration"][finger][1], 
+                                                            self.mcp_joints["current_avg"][finger])
+            
+            self.pip_joints["mcp_adjustment"][finger][0] = max(self.pip_joints["mcp_adjustment"][finger][0], 
+                                                               self.pip_joints["current_avg"][finger])
+            self.pip_joints["mcp_adjustment"][finger][1] = min(self.pip_joints["mcp_adjustment"][finger][1], 
+                                                               self.pip_joints["current_avg"][finger])
+            
+    def update_PIP_extrema(self):
+        for finger in fingers:
+            self.pip_joints["calibration"][finger][0] = max(self.pip_joints["calibration"][finger][0], 
+                                                            self.pip_joints["current_avg"][finger])
+            self.pip_joints["calibration"][finger][1] = min(self.pip_joints["calibration"][finger][1], 
+                                                            self.pip_joints["current_avg"][finger])
+            
+    def linear_fit(self, y2, y1, x2, x1):
+        m = (y2 - y1) / (x2 - x1)
+        b = y2 - m * x2
+        
+        return [m, b]
+    
+    def linear_func(self, x, m, b):
+        return m * x + b
 
     def update_mcp_angles(self, key):
         self.mcp_joints["current_avg"][key] =  (sum(self.mcp_joints["raw"][key]) / len(self.mcp_joints["raw"][key]) + 99) // 100 * 100
@@ -92,37 +161,30 @@ class WBA_Glove:
         
     def update_pip_angles(self, key):
         self.pip_joints["current_avg"][key] =  (sum(self.pip_joints["raw"][key]) / len(self.pip_joints["raw"][key]) + 99) // 100 * 100
+        self.mcp_joints["current_avg"][key] =  (sum(self.mcp_joints["raw"][key]) / len(self.mcp_joints["raw"][key]) + 99) // 100 * 100
         
-        x = self.pip_joints["current_avg"][key]
-        m = self.pip_joints["calibration"][key][2]
-        b = self.pip_joints["calibration"][key][3]
+        x1 = self.pip_joints["current_avg"][key]
+        m1 = self.pip_joints["calibration"][key][2]
+        b1 = self.pip_joints["calibration"][key][3]
         
-        self.pip_joints["angle"][key] = m*x + b
+        x2 = self.mcp_joints["current_avg"][key]
+        m2 = self.pip_joints["mcp_adjustment"][key][2]
+        b2 = self.pip_joints["mcp_adjustment"][key][3]
         
-# Function to send data via serial port
-def send_serial_data(data):
-    uart.write(data)
+        pip = (m1*x1 + b1)
+        pip_error = (m2*x2 + b2)
+
+        if pip > pip_error + 20:
+            self.pip_joints["angle"][key] = pip
+        else:
+            self.pip_joints["angle"][key] = pip - pip_error
 
 if __name__ == "__main__":
-    glove = WBA_Glove()
-        
-    state = ["OPEN", "CLOSE"]
+    glove = WBA_Glove()    
     
-    i=0
-    time = time_ns()
-    while i < 5:
-        glove.read()
-        glove.update_calib_data()
-        
-        if time_ns() - time > 2e9:
-            time = time_ns()
-            print(state[i % 2])
-            i += 1
-            
-    glove.curve_fit()
-        
+    glove.calibrate()
+
     while True:
         glove.read()
-        print(glove.mcp_joints["angle"], end='\r')
-        send_serial_data(str("Hi"))
-        sleep(2)
+        print(glove.pip_joints["angle"])
+        sleep(1e-1)
