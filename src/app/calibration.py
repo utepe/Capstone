0fe40dff23cc46a153e87f43c381ef611bc72da7
Uygur Tepe <uygur.t10@gmail.com>
2023-06-20 15:15:21 -0400

    finished up calibration script

--- src/app/calibration.py
+++ src/app/calibration.py
@@ -1,51 +1,92 @@
 from time import time_ns
 import processing_readings as processing
+from helpers import linear_fit
 
 mcp_joints = processing.mcp_joints
 pip_joints = processing.pip_joints
 fingers = processing.fingers
 
 # update LEDs to indicate calibration mode instead of printing
-def calibration_mode():
+def calibrate():
     state = ("OPEN", "CLOSE")
 
-    print("MCP")
+    print("MCP changing, PIP fixed @ 90")
     i = 0
     time = time_ns()
-    while i < 5:
+    while i < 3:
         processing.read_sensors()
-        update_MCP_max()
+        update_MCP_extrema()
         if time_ns() - time >= 2e9:
             print(i)
             time = time_ns()
             print(state[i % 2])
             i += 1
 
-    print("PIP")
-    i = 0
-    time = time_ns()
-    while i < 5:
-        processing.read_sensors()
-        update_PIP_max()
-        if time_ns() - time >= 2e9:
-            print(i)
-            time = time_ns()
-            print(state[i % 2])
-            i += 1
+    input("Press enter to continue...")
 
     for finger in fingers:
         mcp_joints["calibration"][finger][2], mcp_joints["calibration"][finger][3] = linear_fit(mcp_joints["calibration"][finger][0], mcp_joints["calibration"][finger][1])
 
-        pip_joints["calibration"][finger][2], pip_joints["calibration"][finger][3] = linear_fit(pip_joints["calibration"][finger][0], pip_joints["calibration"][finger][1])
+        pip_joints["raw"][finger] = []
 
-def linear_fit(x1, x2, y1 = 0, y2 = 90):
-    m = (y2 - y1) / (x2 - x1)
-    b = y1 - m * x1
+    print("MCP fixed @ 0, PIP changing")
 
-    return m, b
+    i = 0
+    time = time_ns()
+    while i < 3:
+        processing.read_sensors()
+        for finger in fingers:
+            pip_joints["calibration_0"][finger][0] = max(pip_joints["calibration_0"][finger][0], pip_joints["current_avg"][finger])
 
-def update_PIP_max():
-    pass 
+            pip_joints["calibration_0"][finger][1] = min(pip_joints["calibration_0"][finger][1], pip_joints["current_avg"][finger])
+        
+        if time_ns() - time > 2e9:
+            print(i)
+            time = time_ns()
+            print(state[i % 2])
+            i += 1
 
-def update_MCP_max():
-    pass
+    input("Press enter to continue...")
+
+    for finger in fingers:
+        pip_joints["calibration_0"][finger][2], pip_joints["calibration_0"][finger][3] = linear_fit(pip_joints["calibration_0"][finger][0], pip_joints["calibration_0"][finger][1])
+
+        pip_joints["raw"][finger] = []
+    
+    print("MCP fixed @ 90, PIP changing")
+
+    i = 0
+    time = time_ns()
+    while i < 3:
+        processing.read_sensors()
+        for finger in fingers:
+            pip_joints["calibration_90"][finger][0] = max(pip_joints["calibration_90"][finger][0], pip_joints["current_avg"][finger])
+
+            pip_joints["calibration_90"][finger][1] = min(pip_joints["calibration_90"][finger][1], pip_joints["current_avg"][finger])
+        
+        if time_ns() - time > 2e9:
+            print(i)
+            time = time_ns()
+            print(state[i % 2])
+            i += 1
+
+    input("Press enter to continue...")
+
+    for finger in fingers:
+        pip_joints["calibration_90"][finger][2], pip_joints["calibration_90"][finger][3] = linear_fit(pip_joints["calibration_90"][finger][0], pip_joints["calibration_90"][finger][1])
+
+        pip_joints["raw"][finger] = []
+
+    print("Calibration complete")
+
+def update_PIP_extrema():
+    for finger in fingers:
+        pip_joints["calibration"][finger][0] = max(pip_joints["calibration"][finger][0], pip_joints["current_avg"][finger])
+
+        pip_joints["calibration"][finger][1] = min(pip_joints["calibration"][finger][1], pip_joints["current_avg"][finger]) 
+
+def update_MCP_extrema():
+    for finger in fingers:
+        mcp_joints["calibration"][finger][0] = max(mcp_joints["calibration"][finger][0], mcp_joints["current_avg"][finger])
+
+        mcp_joints["calibration"][finger][1] = min(mcp_joints["calibration"][finger][1], mcp_joints["current_avg"][finger])
