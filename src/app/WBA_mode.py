fdb270b008d9e7c0f48e790198b6d9848df4fa98
Uygur Tepe <uygur.t10@gmail.com>
2023-06-30 21:23:14 -0400

    added WBA mode file

---
+++ src/app/WBA_mode.py
@@ -0,0 +1,6 @@
+import processing_readings as processing
+from helpers import linear_fit
+
+def sendData():
+    # processing.read_sensors()
+    pass
--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,3 +2,4 @@ from processing_readings import *
 from helpers import *
 from unity_mode import *
 from calibration_mode import *
+from WBA_mode import *
