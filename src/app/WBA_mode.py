007c7d7541d26a5e34cf02e64e115fba1d88fb76
faverog <faverog@uwindsor.ca>
2023-07-21 13:24:57 -0400

    cleanup

--- src/app/WBA_mode.py
+++
@@ -1,6 +0,0 @@
-import processing_readings as processing
-from helpers import linear_fit
-
-def sendData():
-    # processing.read_sensors()
-    pass
--- src/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from processing_readings import *
-from helpers import *
-from unity_mode import *
-from calibration_mode import *
-from WBA_mode import *
